b0abcd839bc6df399c0780581894b4712d3f459e
Aditi0901 <74413173+Aditi0901@users.noreply.github.com>
2020-12-21 18:33:19 +0530

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,8 @@
+class Ground extends GroundBaseClass {
+    constructor(x,y,width,height) {
+      super(x, y, width,height);
+      this.image = loadImage ("sprites/platform.png");
+      
+    }
+    
+  }
---
+++ Platform.js
@@ -0,0 +1,9 @@
+class Platform extends GroundBaseClass{
+    constructor(x,y , w, h){
+    super(x, y, w, h);
+    this.image = loadImage("sprites/platform.png");
+
+    }
+
+
+}
